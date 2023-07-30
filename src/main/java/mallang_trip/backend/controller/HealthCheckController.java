87c570d22f96a377e4d7d181d96e19d813528adf
LeeSeongGyu <lsg9706@naver.com>
2023-07-31 00:43:57 +0900

    [FEAT] health check API for load balancer

---
+++ src/main/java/mallang_trip/backend/controller/HealthCheckController.java
@@ -0,0 +1,14 @@
+package mallang_trip.backend.controller;
+
+import mallang_trip.backend.controller.io.BaseResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/check")
+    public BaseResponse<String> healthCheck() {
+        return new BaseResponse<>("OK");
+    }
+}
