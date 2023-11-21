5fed66a3fcca89711b31e7b5f58c356586d82b19
LeeSeongGyu <lsg9706@naver.com>
2023-11-21 16:39:55 +0900

    [FIX]DriverRepository: fix native query

--- src/main/java/mallang_trip/backend/repository/party/PartyRepository.java
+++ src/main/java/mallang_trip/backend/repository/party/PartyRepository.java
@@ -13,14 +13,14 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface PartyRepository extends JpaRepository<Party, Long> {
 
-    @Query(value = "SELECT CASE WHEN COUNT(*) > 0 THEN true ELSE false END FROM party\n"
+    @Query(value = "SELECT CASE WHEN COUNT(*) > 0 THEN 'true' ELSE 'false' END FROM party\n"
         + "WHERE driver_id = :driverId\n"
         + "AND NOT(status = 'CANCELED' OR status = 'DRIVER_REFUSED')\n"
         + "AND start_date = :date", nativeQuery = true)
     Boolean existsValidPartyByDriverAndStartDate(@Param(value = "driverId") Long driverId,
         @Param(value = "date") String startDate);
 
-    @Query(value = "SELECT CASE WHEN COUNT(*) > 0 THEN true ELSE false END \n"
+    @Query(value = "SELECT CASE WHEN COUNT(*) > 0 THEN 'true' ELSE 'false' END \n"
         + "FROM party p JOIN party_members m ON p.id = m.party_id\n"
         + "WHERE m.user_id = :userId AND NOT(status = 'CANCELED' OR status = 'DRIVER_REFUSED') AND p.start_date = :date",
     nativeQuery = true)
