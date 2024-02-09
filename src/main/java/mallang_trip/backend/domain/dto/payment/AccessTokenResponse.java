820e40cafbc1f75c6309d2be5fe2d8c40754e7a5
LeeSeongGyu <lsg9706@naver.com>
2024-02-10 02:13:20 +0900

    [FIX] AccessTokenResponse 빈 생성자 annotation 추가

--- src/main/java/mallang_trip/backend/domain/dto/payment/AccessTokenResponse.java
+++ src/main/java/mallang_trip/backend/domain/dto/payment/AccessTokenResponse.java
@@ -1,10 +1,14 @@
 package mallang_trip.backend.domain.dto.payment;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class AccessTokenResponse {
 
     private String accessToken;
