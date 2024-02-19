1102567221be06e51400c2fd32a704cf642101af
LeeSeongGyu <lsg9706@naver.com>
2024-02-19 16:37:37 +0900

    [FEAT]PaymentController: 결제 재시도 API

--- src/main/java/mallang_trip/backend/domain/payment/controller/PaymentController.java
+++ src/main/java/mallang_trip/backend/domain/payment/controller/PaymentController.java
@@ -10,6 +10,7 @@ import mallang_trip.backend.domain.payment.service.PaymentService;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,28 +22,37 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/payment")
 public class PaymentController {
 
-    private final PaymentService paymentService;
+	private final PaymentService paymentService;
 
-    @ApiOperation(value = "카드 등록")
-    @PostMapping
-    @PreAuthorize("isAuthenticated()") // 로그인 사용자
-    public BaseResponse<CardResponse> save(@RequestParam String authKey)
-        throws BaseException {
-        return new BaseResponse<>(paymentService.register(authKey));
-    }
+	@ApiOperation(value = "카드 등록")
+	@PostMapping
+	@PreAuthorize("isAuthenticated()") // 로그인 사용자
+	public BaseResponse<CardResponse> save(@RequestParam String authKey)
+		throws BaseException {
+		return new BaseResponse<>(paymentService.register(authKey));
+	}
 
-    @ApiOperation(value = "카드 삭제")
-    @DeleteMapping
-    @PreAuthorize("isAuthenticated()") // 로그인 사용자
-    public BaseResponse<String> save() throws BaseException {
-        paymentService.delete();
-        return new BaseResponse<>("성공");
-    }
+	@ApiOperation(value = "카드 삭제")
+	@DeleteMapping
+	@PreAuthorize("isAuthenticated()") // 로그인 사용자
+	public BaseResponse<String> save() throws BaseException {
+		paymentService.delete();
+		return new BaseResponse<>("성공");
+	}
 
-    @ApiOperation(value = "등록된 카드 조회")
-    @GetMapping
-    @PreAuthorize("isAuthenticated()") // 로그인 사용자
-    public BaseResponse<CardResponse> getCard() throws BaseException {
-        return new BaseResponse<>(paymentService.getCard());
-    }
+	@ApiOperation(value = "등록된 카드 조회")
+	@GetMapping
+	@PreAuthorize("isAuthenticated()") // 로그인 사용자
+	public BaseResponse<CardResponse> getCard() throws BaseException {
+		return new BaseResponse<>(paymentService.getCard());
+	}
+
+	@ApiOperation(value = "결제 재시도")
+	@PostMapping("/{reservation_id}")
+	@PreAuthorize("isAuthenticated()") // 로그인 사용자
+	public BaseResponse<String> retryPayment(
+		@PathVariable(value = "reservation_id") Long reservationId) throws BaseException {
+		paymentService.retryPayment(reservationId);
+		return new BaseResponse<>("성공");
+	}
 }
