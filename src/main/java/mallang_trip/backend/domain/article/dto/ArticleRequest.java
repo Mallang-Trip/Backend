57416a0ae7fd7fbfccf32338fd5022717e8c883a
LeeSeongGyu <lsg9706@naver.com>
2024-03-12 16:04:42 +0900

    [REFACTOR] ArticleRequest dto 어노테이션 변경, @ApiModelProperty 작성

--- src/main/java/mallang_trip/backend/domain/article/dto/ArticleRequest.java
+++ src/main/java/mallang_trip/backend/domain/article/dto/ArticleRequest.java
@@ -1,23 +1,33 @@
 package mallang_trip.backend.domain.article.dto;
 
+import io.swagger.annotations.ApiModelProperty;
 import java.util.List;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
-@Builder
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ArticleRequest {
 
-	@NotNull
+	@NotBlank
+	@ApiModelProperty(value = "게시판 타입", required = true)
 	private String type;
 
-	@NotNull
+	@NotBlank
+	@ApiModelProperty(value = "제목", required = true)
 	private String title;
 
-	@NotNull
+	@NotBlank
+	@ApiModelProperty(value = "내용", required = true)
 	private String content;
 
+	@ApiModelProperty(value = "파티 ID", required = false)
 	private Long partyId;
+
+	@ApiModelProperty(value = "이미지 URL 배열", required = false)
 	private List<String> images;
 }
