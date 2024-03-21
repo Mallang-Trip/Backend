320f7fa92046f0e7dfb6351d3e90376d7fbc8b24
LeeSeongGyu <lsg9706@naver.com>
2024-03-21 18:45:32 +0900

    [FEAT] payment domain 삭제, payple domain: 카드 등록,조회,삭제 API & 페이플 파트너인증 요청

---
+++ src/main/java/mallang_trip/backend/domain/payple/dto/PartnerAuthRequest.java
@@ -0,0 +1,14 @@
+package mallang_trip.backend.domain.payple.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class PartnerAuthRequest {
+
+	private String cst_id;
+	private String custKey;
+	private String PCD_PAY_TYPE;
+	private String PCD_SIMPLE_FLAG;
+}
--- src/main/java/mallang_trip/backend/domain/payple/repository/CardRepository.java
+++ src/main/java/mallang_trip/backend/domain/payple/repository/CardRepository.java
@@ -1,13 +1,13 @@
-package mallang_trip.backend.domain.payment.repository;
+package mallang_trip.backend.domain.payple.repository;
 
 import java.util.Optional;
-import mallang_trip.backend.domain.payment.entity.Card;
-import mallang_trip.backend.domain.payment.entity.Payment;
+import mallang_trip.backend.domain.payple.entity.Card;
+import mallang_trip.backend.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CardRepository extends JpaRepository<Card, Long> {
 
-    Optional<Card> findByPayment(Payment payment);
+	Optional<Card> findByUser(User user);
 }
