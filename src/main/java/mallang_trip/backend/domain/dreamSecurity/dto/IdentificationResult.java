00a80a789ace313ef1fd13e7d98a8a7d23a8b448
LeeSeongGyu <lsg9706@naver.com>
2024-08-04 15:35:44 +0900

    [FIX] IdentificationResult dto: LocalDate JSON 역직렬화 오류 수정

--- src/main/java/mallang_trip/backend/domain/dreamSecurity/dto/IdentificationResult.java
+++ src/main/java/mallang_trip/backend/domain/dreamSecurity/dto/IdentificationResult.java
@@ -1,5 +1,9 @@
 package mallang_trip.backend.domain.dreamSecurity.dto;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import java.time.LocalDate;
 import lombok.Builder;
 import lombok.Getter;
@@ -12,7 +16,10 @@ public class IdentificationResult {
 	private String ci;
 	private String di;
 	private String userPhone;
-	private LocalDate userBirthday;
 	private String userGender;
 	private String userNation;
+
+	@JsonSerialize(using = LocalDateSerializer.class)
+	@JsonDeserialize(using = LocalDateDeserializer.class)
+	private LocalDate userBirthday;
 }
