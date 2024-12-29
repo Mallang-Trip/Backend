d7eb9f779f9eb358fe61466182228deed9dd364e
KAN-JUNHO <junho6667@gmail.com>
2024-12-29 16:58:29 +0900

    드라이버 프로필 전체 조회 수정
    (cherry picked from commit b4ca0c5cdbdc0e48ecf1c3a322563f32252d9824)

--- src/main/java/mallang_trip/backend/domain/driver/dto/DriverBriefResponse.java
+++ src/main/java/mallang_trip/backend/domain/driver/dto/DriverBriefResponse.java
@@ -4,6 +4,8 @@ import lombok.Builder;
 import lombok.Getter;
 import mallang_trip.backend.domain.driver.entity.Driver;
 
+import java.util.List;
+
 @Getter
 @Builder
 public class DriverBriefResponse {
@@ -11,12 +13,14 @@ public class DriverBriefResponse {
     private Long driverId;
     private String name;
     private String profileImg;
+    private List<String> region;
 
     public static DriverBriefResponse of(Driver driver){
         return DriverBriefResponse.builder()
             .driverId(driver.getId())
             .name(driver.getUser().getName())
             .profileImg(driver.getUser().getProfileImage())
+            .region(driver.getRegion())
             .build();
     }
 }
